8d939d42eb3bb25cc02a28df3d576301f76b8bba
Benjamin Mahr <ben.mahr@gmail.com>
2019-11-18 21:10:47 +0100

    cleanup

--- luDecomposition.h
+++ luDecomposition.h
@@ -26,8 +26,6 @@ namespace LUDecomposition {
         decomposition.L = MatrixFactory::IdentityMatrix<T>(nbRows);
 
         for(size_t column = 0; column < nbColumns; ++column) {
-            //decomposition.L(column, column) = 1;
-
             for(size_t row = column + 1; row < nbRows; ++row) {
                 const T & divisor = decomposition.U(column, column);
                 if(std::fabs(divisor) < std::numeric_limits<T>::min()) {
